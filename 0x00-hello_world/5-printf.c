2aa55292fc7a96e53ec40f088463a0af660a0805
Ahmed Awad <ahmedrhama99@gmail.com>
2022-09-11 05:53:29 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main function is 
+ * fhsuf 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}	
