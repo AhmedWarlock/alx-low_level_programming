859d23228aa45f1465721dbd93f1a324df6fa72e
Ahmed Awad <ahmedrhama99@gmail.com>
2022-09-11 06:09:08 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
 /**
- * main funcd
+ *main - print the string in the function
+ *
+ * Description: using the main function
+ * this prgram prints the phrase Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }	
