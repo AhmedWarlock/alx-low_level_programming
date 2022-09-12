d48be4ced28d0fa735367e8a5156330e6a849a31
Ahmed Awad <ahmedrhama99@gmail.com>
2022-09-12 09:54:29 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this programm
+ *
+ * Description: this program
+ * using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; c <= 'z' ; ch++)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
