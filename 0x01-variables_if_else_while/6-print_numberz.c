556db93dbd622eb8f297f2574928ebac6cbcd7dd
Ahmed Awad <ahmedrhama99@gmail.com>
2022-09-12 10:28:53 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - ff
+ *
+ * Description: ff
+ * fff
+ * Return: 0
+ */
+int main(void)
+{
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - fff
+ *
+ * Description: fff
+ * ffff
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
